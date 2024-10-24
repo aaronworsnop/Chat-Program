a7c03ed32e8cd8c3dbff01e5eb47124de61eee03
aaronworsnop <aaronworsnop@gmail.com>
2024-10-24 16:45:12 +1300

    style: Better codestyle and comments for client

--- chat_client.py
+++ chat_client.py
@@ -12,6 +12,7 @@ stop_thread = False
 
 
 def get_and_send(client):
+    """Get user input and send to server."""
     while not stop_thread:
         data = sys.stdin.readline().strip()
         if data:
@@ -19,21 +20,21 @@ def get_and_send(client):
 
 
 class ChatClient():
-    """ A command line chat client using select """
+    """Chat client using select"""
 
     def __init__(self, port, host=SERVER_HOST):
         self.connected = False
         self.host = host
         self.port = port
 
+        # Set up SSL context for secure communication
         self.context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         self.context.set_ciphers('AES128-SHA')
 
-        # Connect to server at port
+        # Connect to server at given host and port
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock = self.context.wrap_socket(
-                self.sock, server_hostname=host)
+            self.sock = self.context.wrap_socket(self.sock, server_hostname=host)
             self.sock.connect((host, self.port))
             print(f'Now connected to chat server@ port {self.port}')
             self.connected = True
@@ -47,6 +48,7 @@ class ChatClient():
             # Process username input
             username_prompt = receive(self.sock)
 
+            # Check if client selected "register" or "login" correctly
             if 'Invalid' in username_prompt:
                 print(username_prompt)
                 self.connected = False
@@ -60,10 +62,13 @@ class ChatClient():
             # Check if registration/login was successful before setting the username
             msg = receive(self.sock)
             print(msg)
-            if 'Welcome' in msg:  # success message contains 'Welcome'
+
+            if 'Welcome' in msg:  
+                # The user is now logged in
                 self.prompt = f''
                 threading.Thread(target=get_and_send, args=(self,)).start()
             else:
+                # The user failed to log in or register
                 print("Failed to log in or register. Exiting.")
                 self.connected = False
                 self.sock.close()
@@ -77,7 +82,7 @@ class ChatClient():
         self.sock.close()
 
     def run(self):
-        """ Chat client main loop """
+        """Chat client main loop."""
         while self.connected:
             try:
                 sys.stdout.write(self.prompt)
@@ -85,6 +90,7 @@ class ChatClient():
 
                 readable, _, _ = select.select([self.sock], [], [])
 
+                # Handle incoming messages
                 for sock in readable:
                     if sock == self.sock:
                         data = receive(self.sock)
@@ -96,6 +102,7 @@ class ChatClient():
                             sys.stdout.write(data + '\n')
                             sys.stdout.flush()
 
+            # Handle keyboard interrupts
             except KeyboardInterrupt:
                 print(" Client interrupted.")
                 self.cleanup()
@@ -103,6 +110,7 @@ class ChatClient():
 
 
 if __name__ == "__main__":
+    """Parse command line arguments and start the chat client."""
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', action="store",
                         dest="port", type=int, required=True)
